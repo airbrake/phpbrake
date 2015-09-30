623496086e097a0535bc651cff5d4360aa6e430b
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2015-09-30 16:24:55 +0300

    Extract Troublemaker class so don't need to update lines every time tests are changed.

--- tests/NotifierTest.php
+++ tests/NotifierTest.php
@@ -147,7 +147,7 @@ class OnErrorTest extends \PHPUnit_Framework_TestCase
         $handler = new \Airbrake\ErrorHandler($this->notifier);
         $handler->register();
 
-        echo $undefined_var;
+        Troublemaker::echoUndefinedVar();
     }
 
     public function testPostsError()
@@ -155,22 +155,22 @@ class OnErrorTest extends \PHPUnit_Framework_TestCase
         $notice = $this->notifier->notice;
         $error = $notice['errors'][0];
         $this->assertEquals('Airbrake\Errors\Notice', $error['type']);
-        $this->assertEquals('Undefined variable: undefined_var', $error['message']);
+        $this->assertEquals('Undefined variable: undefinedVar', $error['message']);
     }
 
     public function testPostsBacktrace()
     {
         $backtrace = $this->notifier->notice['errors'][0]['backtrace'];
-        $this->assertCount(10, $backtrace);
+        $this->assertCount(12, $backtrace);
 
         $wanted = [[
-            'file' => dirname(dirname(__FILE__)).'/tests/NotifierTest.php',
-            'line' => 150,
-            'function' => 'Airbrake\Tests\OnErrorTest->setUp',
+            'file' => dirname(dirname(__FILE__)).'/tests/Troublemaker.php',
+            'line' => 9,
+            'function' => 'Airbrake\Tests\Troublemaker::doEchoUndefinedVar',
         ], [
-            'file' => dirname(dirname(__FILE__)).'/vendor/phpunit/phpunit/src/Framework/TestCase.php',
-            'line' => 742,
-            'function' => 'PHPUnit_Framework_TestCase->runBare',
+            'file' => dirname(dirname(__FILE__)).'/tests/Troublemaker.php',
+            'line' => 14,
+            'function' => 'Airbrake\Tests\Troublemaker::echoUndefinedVar',
         ]];
         for ($i = 0; $i < count($wanted); $i++) {
             $this->assertEquals($wanted[$i], $backtrace[$i]);
