e6e8701ba272f4fb99808c2f5de8b07a64f2ff09
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2017-09-11 13:22:12 +0300

    Add async API using Guzzle promises

--- tests/NotifierMock.php
+++ tests/NotifierMock.php
@@ -3,27 +3,33 @@
 namespace Airbrake\Tests;
 
 use Airbrake\Notifier;
+use GuzzleHttp\Promise\FulfilledPromise;
 
 class NotifierMock extends Notifier
 {
     public $url;
-    public $data;
     public $notice;
-
     public $resp;
 
     public function __construct($opt)
     {
         parent::__construct($opt);
         $this->resp = new ResponseMock(201, '{"id":"12345"}');
-        ;
     }
 
-    public function postNotice($url, $data)
+    public function sendRequest($req)
     {
-        $this->url = $url;
-        $this->data = $data;
+        $this->url = (string) $req->getUri();
+        $data = $req->getBody()->getContents();
         $this->notice = json_decode($data, true);
         return $this->resp;
+    }
+
+    public function sendRequestAsync($req)
+    {
+        $this->url = (string) $req->getUri();
+        $data = $req->getBody()->getContents();
+        $this->notice = json_decode($data, true);
+        return new FulfilledPromise($this->resp);
     }
 }
