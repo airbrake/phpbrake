19d06e2f787b2600c64ff4db9af8181155e5f7fb
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2020-01-15 09:51:01 +0200

    v0.7.2

--- src/Notifier.php
+++ src/Notifier.php
@@ -103,7 +103,7 @@ class Notifier
         $context = [
             'notifier' => [
                 'name' => 'phpbrake',
-                'version' => '0.7.1',
+                'version' => '0.7.2',
                 'url' => 'https://github.com/airbrake/phpbrake',
             ],
             'os' => php_uname(),
